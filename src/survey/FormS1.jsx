ffbb8cbef0ad54922d922b9204158992d0b94048
Ratnesh Raval <ratneshraval@gmail.com>
2019-02-04 15:50:00 -0800

    initial commit

---
+++ src/survey/FormS1.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import * as Survey from 'survey-react'
+import 'survey-react/survey.css'
+
+export default class FormS1 extends Component {
+  render () {
+    return (
+      <p> NESH </p>
+    )
+  }
+}
