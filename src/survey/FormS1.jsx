42b1ed7418be01ea6b33f74ea7b219a64043c60e
Ratnesh Raval <ratneshraval@gmail.com>
2019-02-06 16:03:08 -0800

    Addig phone numbers - matrixdynamic

--- src/survey/FormS1.jsx
+++ src/survey/FormS1.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import * as Survey from 'survey-react'
 import 'survey-react/survey.css'
+import './forms1.css'
 
 export default class FormS1 extends Component {
   json = {
     checkErrorsMode: 'onComplete',
     completeText: 'Save / Submit',
+    questionErrorLocation: 'bottom',
     elements: [
 
       {
         type: 'paneldynamic',
-        innerIndent: 1,
+        innerIndent: 2,
+        minPanelCount: 1,
+        panelRemoveText: 'Remove Applicant',
         name: 'applicants',
         title: 'Applicant Information',
         templateElements: [
@@ -26,7 +30,7 @@ export default class FormS1 extends Component {
             ],
             width: '30%'
           },
-          { type: 'text', name: 'first_name', title: 'First Name', isRequired: true },
+          { type: 'text', name: 'first_name', title: 'First Name', isRequired: true, text: 'Enter first name' },
           { type: 'text', name: 'middle_name', title: 'Middle Name', startWithNewLine: false },
           { type: 'text', name: 'last_name', title: 'Last Name', isRequired: true, startWithNewLine: false },
           {
@@ -55,16 +59,83 @@ export default class FormS1 extends Component {
       },
 
       {
-        name: 'number',
-        type: 'text',
-        inputType: 'tel',
-        title: 'Phone Number',
-        isRequired: true,
-        validators: [{ 'type': 'numeric' }],
-        width: '30%',
-        maxLength: 10
-
+        type: 'matrixdynamic',
+        // minPanelCount: 1,
+        // panelRemoveText: 'Remove Phone',
+        name: 'phones',
+        title: 'Phone numbers',
+        columns: [
+          {
+            cellType: 'dropdown',
+            name: 'type',
+            title: 'Type',
+            choices: [
+              'Cellular',
+              'Home',
+              'Work'
+            ],
+            minWidth: '20%'
+          },
+          {
+            name: 'number',
+            cellType: 'text',
+            inputType: 'tel',
+            title: 'Phone Number',
+            // isRequired: true,
+            // validators: [{ 'type': 'numeric' }],
+            width: '30%',
+            maxLength: 10
+            // startWithNewLine: false
+          },
+          {
+            name: 'preferred',
+            cellType: 'checkbox',
+            title: 'Preferred',
+            choices: ['Preferred'],
+            startWithNewLine: false
+          }
+        ]
       }
+
+      // {
+      //   type: 'paneldynamic',
+      //   innerIndent: 2,
+      //   minPanelCount: 1,
+      //   panelRemoveText: 'Remove Phone',
+      //   name: 'phones',
+      //   title: 'Phone numbers',
+      //   templateElements: [
+      //     {
+      //       type: 'dropdown',
+      //       name: 'type',
+      //       title: 'Type',
+      //       choices: [
+      //         'Cellular',
+      //         'Home',
+      //         'Work'
+      //       ],
+      //       width: '20%'
+      //     },
+      //     {
+      //       name: 'number',
+      //       type: 'text',
+      //       inputType: 'tel',
+      //       title: 'Phone Number',
+      //       isRequired: true,
+      //       validators: [{ 'type': 'numeric' }],
+      //       width: '30%',
+      //       maxLength: 10,
+      //       startWithNewLine: false
+      //     },
+      //     {
+      //       name: 'preferred',
+      //       type: 'checkbox',
+      //       title: 'Preferred',
+      //       choices: ['Preferred'],
+      //       startWithNewLine: false
+      //     }
+      //   ]
+      // }
 
     ],
     completeSurveyText: 'Save'
@@ -76,8 +147,8 @@ export default class FormS1 extends Component {
   }
 
   render () {
-    // Survey.StylesManager
-    //   .applyTheme('default')
+    Survey.StylesManager
+      .applyTheme('winterstone')
     const styles = {
       width: '70%',
       margin: 'auto'
